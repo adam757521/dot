c027ae7faa5e09a94f4677f6801f14b39a666136
adam7100 <73548219+adam757521@users.noreply.github.com>
2025-08-02 07:24:08 +0300

    Add files via upload

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"mason-org/mason.nvim",
+				config = function()
+					local mason = require("mason")
+					mason.setup()
+				end
+			},
+			{ 
+				"mason-org/mason-lspconfig.nvim",
+				config = function() 
+					local mason_lspconfig = require("mason-lspconfig")
+					mason_lspconfig.setup({})
+				end 
+			},
+		},
+		config = function ()
+			local lspconfig = require("lspconfig")
+		end
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy=false,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = { enable = true, additional_vim_regex_highlighting = false, },
+				indent = { enable = true },
+			})
+
+		end
+
+	},
+}
+	
